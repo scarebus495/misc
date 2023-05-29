29dc6daf2880908c50131716fffb1a8636ce3074
scarebus495 <anr31415@gmail.com>
2023-05-28 21:51:27 -0600

    sweeet

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 //===================FUNCTIONS GO HERE==========================================================
 
 
-bool guess_function(std::vector<char> word, std::vector<char> reveal, char guess){
+bool guess_function(std::vector<char> word, char guess){
     for(int i = 0; i < word.size(); i++) {
         if(word[i] == guess) {
             return true;
@@ -14,69 +14,102 @@ bool guess_function(std::vector<char> word, std::vector<char> reveal, char guess
     return false;
 }
 
+void greet() {
+    std::cout << "\n======================\n";
+    std::cout << "UFO: THE GAME";
+    std::cout << "\n======================\n";
+    std::cout << "Instructions: Player One will type a word, Player two will guess the word\n";
+    std::cout << "PLAYER ONE: CHOOSE A WORD (USE ALL CAPS) *PLAYER TWO LOOK AWAY* : ";
+}
 
+bool end_check(std::vector<char> reveal, std::vector<char> word) {
+    if(reveal == word) {
+        return true;
+    }
+    return false;
+}
 
+void end_good() {
+    std::cout << "\nCONGRATS YOU WIN\n";
+}
 
+void end_bad() {
+    std::cout << "\nYA FACKIN BLEW IT\n";
+}
 
+std::vector<char> replace(std::vector<char> word, std::vector<char> reveal, char guess) {
+    for(int i = 0; i < word.size(); i++) {
+        if(word[i] == guess) {
+            reveal[i] = guess;
+        }
+        std::cout << " " << reveal[i];
+    }
+    return reveal;
+}
+
+std::vector<char> init_word(std::string word_init) {
+    std::vector<char> word(word_init.size());
+    for(int i = 0; i < word_init.size(); i++) {
+        word[i] = word_init[i];
+    }
+    return word;
+}
+
+std::vector<char> init_reveal(std::vector<char> word) {
+    std::vector<char> reveal(word.size());
+    for(int i = 0; i < word.size(); i++) {
+        reveal[i] = '_';
+    }
+}
 //===============================================================================================
 int main () {
     std::string word_init;
     int miss = 0;
     char guess;
-    std::cout << "PLAYER ONE: CHOOSE A WORD (USE ALL CAPS) *PLAYER TWO LOOK AWAY* : ";
-    std::cin >> word_init;
+    //INTRO
+    greet();
+    std::cin >> word_init; //PLAYER ONE INPUTS WORD    
+    std::vector<char> word(word_init.size()); //
+    std::vector<char> reveal(word_init.size());
     
-    std::vector<char> word(word_init.size());
-    std::vector <char> reveal(word_init.size());
-    
+
     //CONVERTS ENTERED WORD INTO A VECTOR
-    if(word_init.size() > 1) {
-        for(int i = 0; i < word_init.size(); i++) {
-            word[i] = word_init[i];
-        }
+
+    word = init_word(word_init);
+
+    //CONVERTS WORD TO REVEAL WITH SPACES INSTEAD
+
     
-        //CONVERTS WORD TO REVEAL WITH SPACES INSTEAD
-        for(int i = 0; i < word.size(); i++) {
-            reveal[i] = '_';
-        }
-        //SEPERATES
-        std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-        //PRINT OUT REVEAL (SHOULD BE ALL '_')
-        for(char reveal : reveal) {
-            std::cout << reveal << " ";
-        }
-        std::cout << "\nPLAYER TWO GUESS A LETTER (CAPS) : ";
+    /*
+    for(int i = 0; i < word.size(); i++) {
+        reveal[i] = '_';
     }
-    
-while(miss < 7) {
-    
+    */
+    //SEPERATES
+    for(int i = 0; i < 200; i++) {
+        std::cout << "\n";
+    }
+    //PRINT OUT REVEAL (SHOULD BE ALL '_')
+    for(char reveal : reveal) {
+        std::cout << reveal << " ";
+    }
 
-    std::cin >> guess;
-
-    if(guess_function(word, reveal, guess)) {
-        std::cout << "\nYUP\n";
-        //replace function here
-        for(int i = 0; i < word.size(); i++) {
-            if(word[i] == guess) {
-                reveal[i] = guess;
-            }
-        }
-        //reveal function here
-        for(char reveal : reveal) {
-            std::cout << reveal << " ";
-        }
-    } else {
+    std::cout << "\nPLAYER TWO GUESS A LETTER (CAPS) : ";
+    while(miss < 7) {
+        std::cout << "\n";
+        std::cin >> guess;
+        if(guess_function(word, guess)) {
+            std::cout << "\nYUP\n";
+            reveal = replace(word, reveal, guess);
+        } else {
             miss++;
             std::cout << "NOPE\n";
         }
-    //check function
-    if(reveal == word) {
-        //PLAY CONGRATS 
-        std::cout << "\nCONGRATS YOU WIN\n";
-        return 0;
+        if(end_check(reveal, word)) {
+            end_good();
+        }
     }
-}
-std::cout << "\nYA BLEW IT\n";
+    end_bad();
 }
 
 
